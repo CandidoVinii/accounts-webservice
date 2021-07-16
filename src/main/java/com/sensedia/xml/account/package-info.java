6044701fd4adb5ad3396e5bb09744e3cc56cb38a
lucas-m-xavier <zallius@hotmail.com>
2021-07-16 14:24:24 -0300

    classes java

---
+++ src/main/java/com/sensedia/xml/account/package-info.java
@@ -0,0 +1,9 @@
+//
+// Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.3.2 
+// Consulte <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
+// Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
+// Gerado em: 2021.07.15 às 12:43:28 PM BRT 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.sensedia.com/xml/account", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.sensedia.xml.account;
